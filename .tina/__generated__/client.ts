bcb402342b521829511ab2b9fe1c55ed7fdc4fd1
Guillaume <g.kempfer@inforca.mc>
2022-09-04 03:58:43 +0200

    add .tina generated files

---
+++ .tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'https://content.tinajs.io/content/b59b500a-74ef-4403-a433-ef981c3f53ee/github/main', token: 'a06bdcf8a62c91081c150e2578324039d898d2f8', queries });
+export default client;
+  
