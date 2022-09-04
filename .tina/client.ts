852ca6f936c851f32aa3f3f4476dd04199d3125a
Guillaume <g.kempfer@inforca.mc>
2022-09-04 03:19:32 +0200

    force apiURL

--- .tina/client.ts
+++ .tina/client.ts
@@ -2,10 +2,7 @@ import { createClient } from "tinacms/dist/client";
 import { queries } from './__generated__/types'
 
 const branch = "main";
-const apiURL =
-    process.env.NODE_ENV == "development"
-        ? "http://localhost:4001/graphql"
-        : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
+const apiURL = `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
 
 console.log('env', process.env.NODE_ENV);
 
