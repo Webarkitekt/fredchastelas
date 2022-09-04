57674fddccdd10df511d79739330131fdb1e2342
Guillaume <g.kempfer@inforca.mc>
2022-09-04 03:15:59 +0200

    node_env fix

--- .tina/client.ts
+++ .tina/client.ts
@@ -7,5 +7,7 @@ const apiURL =
         ? "http://localhost:4001/graphql"
         : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
 
+console.log('env', process.env.NODE_ENV);
+
 // Token generated on app.tina.io
 export const client = createClient({ queries, url: apiURL, token: 'f050b0efc727e82b3552ee0c24a6da34a0286016'});
